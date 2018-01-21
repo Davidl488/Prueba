dbd356d8f1d2942e0a15252cec5d38e52f8478d3
Davidl88 <davidlopez488@gmail.com>
2018-01-21 11:36:55 -0300

    prueba

---
+++ contact.php
@@ -0,0 +1,237 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>ADD</title>
+    <link rel="shortcut icon" href="images/icono.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Html5TemplatesDreamweaver.com">
+
+    <link href="scripts/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="scripts/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Icons -->
+    <link href="scripts/icons/general/stylesheets/general_foundicons.css" media="screen" rel="stylesheet" type="text/css" />  
+    <link href="scripts/icons/social/stylesheets/social_foundicons.css" media="screen" rel="stylesheet" type="text/css" />
+    <!--[if lt IE 8]>
+        <link href="scripts/icons/general/stylesheets/general_foundicons_ie7.css" media="screen" rel="stylesheet" type="text/css" />
+        <link href="scripts/icons/social/stylesheets/social_foundicons_ie7.css" media="screen" rel="stylesheet" type="text/css" />
+    <![endif]-->
+    <link rel="stylesheet" href="scripts/fontawesome/css/font-awesome.min.css">
+    <!--[if IE 7]>
+        <link rel="stylesheet" href="scripts/fontawesome/css/font-awesome-ie7.min.css">
+    <![endif]-->
+
+    <link href="scripts/wookmark/css/style.css" rel="stylesheet" type="text/css" />	<link href="scripts/yoxview/yoxview.css" rel="stylesheet" type="text/css" />
+    <link href="http://fonts.googleapis.com/css?family=Syncopate" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Abel" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Pontano+Sans" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet" type="text/css">
+
+    <link href="styles/custom.css" rel="stylesheet" type="text/css" />
+    <script src="email/validation.js" type="text/javascript"></script>
+</head>
+<body id="pageBody">
+
+<div id="divBoxed" class="container">
+
+    <div class="transparent-bg" style="position: absolute;top: 0;left: 0;width: 100%;height: 100%;z-index: -1;zoom: 1; background-image: url(styles/fondo.png); background-repeat:no-repeat;" ></div>
+
+    <div class="divPanel notop nobottom">
+            <div class="row-fluid">
+                <div class="span12">
+
+                    <div id="divLogo" class="pull-left">
+                        <img src="images/add.png">
+                    </div>
+
+                    <div id="divMenuRight" class="pull-right">
+                    <div class="navbar">
+                        <button type="button" class="btn btn-navbar-highlight btn-large btn-primary" data-toggle="collapse" data-target=".nav-collapse">
+                            NAVIGATION <span class="icon-chevron-down icon-white"></span>
+                        </button>
+                        <div class="nav-collapse collapse">
+                            <ul class="nav nav-pills ddmenu">
+                            <li><a href="index.html">Inicio</a></li>
+                            <li class="dropdown">
+                            <li><a href="services.html">Producto</a></li>
+                            <li><a href="portfolio.html">Opcionales</a></li>
+                            <li class="active"><a href="contact.php">Contacto</a></li>
+                            </ul>
+                            </div>
+                    </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="row-fluid">
+                <div class="span12">
+                    <div id="contentInnerSeparator"></div>
+                </div>
+            </div>
+    </div>
+
+    <div class="contentArea">
+
+        <div class="divPanel notop page-content">
+
+            <div class="breadcrumbs">
+                <a href="index.html">Inicio</a> &nbsp;/&nbsp; <span>Contacto</span>
+            </div>
+            	
+            <div class="row-fluid">
+                <div class="span8" id="divMain">
+
+                    <h1>Contáctenos</h1>
+                   	<h3 style="color:#FF6633;"><?php echo @$_GET['msg'];?></h3>
+					<hr>
+			<!--Start Contact form -->		                                                
+<form name="enq" method="post" action="email/" onsubmit="return validation();">
+  <fieldset>
+    
+	<input type="text" name="name" id="name" value=""  class="input-block-level" placeholder="Nombre" />
+    <input type="text" name="email" id="email" value="" class="input-block-level" placeholder="Email" />
+    <textarea rows="11" name="message" id="message" class="input-block-level" placeholder="Comentarios"></textarea>
+    <div class="actions">
+	<input type="submit" value="Enviar tu mensaje" name="submit" id="submitButton" class="btn btn-info pull-right" title="Click aquí para enviar tu mensaje!" />
+	</div>
+	
+	</fieldset>
+</form>  				 
+			<!--End Contact form -->											 
+                </div>
+				
+			<!--Edit Sidebar Content here-->	
+                <div class="span4 sidebar">
+
+                    <div class="sidebox">
+                        <h3 class="sidebox-title">Información de Contacto</h3>
+                    <p>
+                        <address><strong>ADD - CNC</strong><br />
+                        Dirección<br />
+                        San Francisco, Córdoba, 2400<br />
+                        <abbr>Tel.:</abbr> (03564) 15628609</address> 
+                        <address>  <strong>Email</strong><br />
+                        <a href="mailto:#">addacanaladoras@hotmail.com</a></address>  
+                    </p>     
+                     
+					 <!-- Start Side Categories 
+        <h4 class="sidebox-title">Sidebar Categories</h4>
+        <ul>
+          <li><a href="#">Quisque diam lorem sectetuer adipiscing</a></li>
+          <li><a href="#">Interdum vitae, adipiscing dapibus ac</a></li>
+          <li><a href="#">Scelerisque ipsum auctor vitae, pede</a></li>
+          <li><a href="#">Donec eget iaculis lacinia non erat</a></li>
+          <li><a href="#">Lacinia dictum elementum velit fermentum</a></li>
+          <li><a href="#">Donec in velit vel ipsum auctor pulvinar</a></li>
+        </ul>
+					End Side Categories -->
+                    					
+                    </div>
+					
+					
+                    
+                </div>
+			<!--/End Sidebar Content-->
+				
+				
+            </div>			
+
+            <div id="footerInnerSeparator"></div>
+        </div>
+    </div>
+
+    <div id="footerOuterSeparator"></div>
+
+    <div id="divFooter" class="footerArea">
+
+        <div class="divPanel">
+
+            <div class="row-fluid">
+                <div class="span4" id="footerArea1">
+                
+                    <h3>EMPRESA</h3>
+
+                    <p>ADD produce y comercializa maquinas con control numerico computarizado. Nos presentamos como una empresa joven con solo cuatro años en el mercado superado las 200 unidades construidas. El equipamiento y la tecnología para la fabricación de productos confiables y de calidad fueron volcados íntegramente en la fabricación de los productos ADD.</p>
+
+                </div>
+                <div class="span4" id="footerArea2">
+
+                    <h3>EQUIPAMIENTO</h3> 
+                    <p>
+                        <a href="services.html" title="">Productos</a><br />
+                    </p>
+                    <p>
+                        <a href="portfolio.html" title="">Opcionales</a><br />
+                    </p>
+                </div>
+                <div class="span4" id="footerArea4">
+
+                    <h3>CONTACTO</h3>  
+                                                               
+                    <ul id="contact-info">
+                    <li>                                    
+                        <i class="general foundicon-phone icon"></i>
+                        <span class="field">Teléfono Celular:</span>
+                        <br />
+                        (03564) 15628609                                                                     
+                    </li>
+                    <li>
+                        <i class="general foundicon-mail icon"></i>
+                        <span class="field">Email:</span>
+                        <br />
+                        <a href="mailto:addacanaladoras@hotmail.com" title="Email">addacanaladoras@hotmail.com</a>
+                    </li>
+                    <li>
+                        <i class="general foundicon-home icon" style="margin-bottom:50px"></i>
+                        <span class="field">Ubicación:</span>
+                        <br />
+                        2400 San Francisco, Córdoba<br />
+                        Argentina
+                    </li>
+                    </ul>
+
+                </div>
+            </div>
+
+            <br /><br />
+            <div class="row-fluid">
+                <div class="span12">
+                    <p class="copyright">
+                        Copyright © ADD. Todos los derechos reservados.
+                    </p>
+
+                    <p class="social_bookmarks">
+                        <a href="#"><i class="social foundicon-facebook"></i> Facebook</a>
+                         <a href=""><i class="social foundicon-twitter"></i> Twitter</a>
+                         <a href=""><i class="social foundicon-youtube"></i> Youtube</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<br /><br /><br />
+
+<script src="scripts/jquery.min.js" type="text/javascript"></script> 
+<script src="scripts/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="scripts/default.js" type="text/javascript"></script>
+
+<script src="scripts/wookmark/js/jquery.wookmark.js" type="text/javascript"></script>
+<script type="text/javascript">$(window).load(function () {var options = {autoResize: true,container: $('#gridArea'),offset: 10};var handler = $('#tiles li');handler.wookmark(options);$('#tiles li').each(function () { var imgm = 0; if($(this).find('img').length>0)imgm=parseInt($(this).find('img').not('p img').css('margin-bottom')); var newHeight = $(this).find('img').height() + imgm + $(this).find('div').height() + $(this).find('h4').height() + $(this).find('p').not('blockquote p').height() + $(this).find('iframe').height() + $(this).find('blockquote').height() + 5;if($(this).find('iframe').height()) newHeight = newHeight+15;$(this).css('height', newHeight + 'px');});handler.wookmark(options);handler.wookmark(options);});</script>
+<script src="scripts/yoxview/yox.js" type="text/javascript"></script>
+<script src="scripts/yoxview/jquery.yoxview-2.21.js" type="text/javascript"></script>
+<script type="text/javascript">$(document).ready(function () {$('.yoxview').yoxview({autoHideInfo:false,renderInfoPin:false,backgroundColor:'#ffffff',backgroundOpacity:0.8,infoBackColor:'#000000',infoBackOpacity:1});$('.yoxview a img').hover(function(){$(this).animate({opacity:0.7},300)},function(){$(this).animate({opacity:1},300)});});</script>
+
+
+</body>
+</html>
